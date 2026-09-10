5161f45d0aa18657c2ae63a82ebcb7848e5175db
carolineward2 <cward1@companieshouse.gov.uk>
2026-09-10 09:19:34 +0100

    SIV-1240 Validate company_upgraded_auth_valid_until on sensitive endpoints

--- src/main/java/uk/gov/companieshouse/accounts/association/configuration/InterceptorConfig.java
+++ src/main/java/uk/gov/companieshouse/accounts/association/configuration/InterceptorConfig.java
@@ -4,19 +4,24 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import uk.gov.companieshouse.accounts.association.interceptor.RequestLifecycleInterceptor;
+import uk.gov.companieshouse.accounts.association.interceptor.TokenPermissionsInterceptor;
 import uk.gov.companieshouse.accounts.association.service.UsersService;
 
 @Configuration
 public class InterceptorConfig implements WebMvcConfigurer {
 
     private final UsersService usersService;
+    private final TokenPermissionsInterceptor tokenPermissionsInterceptor;
 
-    public InterceptorConfig( final UsersService usersService ) {
+    public InterceptorConfig( final UsersService usersService, final TokenPermissionsInterceptor tokenPermissionsInterceptor ) {
         this.usersService = usersService;
+        this.tokenPermissionsInterceptor = tokenPermissionsInterceptor;
     }
 
     @Override
     public void addInterceptors( final InterceptorRegistry registry ) {
+        registry.addInterceptor( tokenPermissionsInterceptor )
+                .addPathPatterns( "/associations", "/associations/*" );
         registry.addInterceptor( new RequestLifecycleInterceptor( usersService ) );
     }
 
