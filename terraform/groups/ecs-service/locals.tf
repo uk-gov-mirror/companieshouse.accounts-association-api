e13a810686d1c01a676cec5aec1d4e044457f5f3
bcullerton <bcullerton@companieshouse.gov.uk>
2026-09-11 08:07:08 +0100

    Rework pstore lookup to pull from vault

--- terraform/groups/ecs-service/locals.tf
+++ terraform/groups/ecs-service/locals.tf
@@ -18,18 +18,11 @@ locals {
   use_set_environment_files  = var.use_set_environment_files
   s3_config_bucket           = data.vault_generic_secret.shared_s3.data["config_bucket_name"]
   app_environment_filename   = "accounts-association-api.env"
-  vpc_name                   = data.aws_ssm_parameter.secret[format("/%s/%s", local.name_prefix, "vpc-name")].value
+  vpc_name                   = local.stack_secrets["vpc_name"]
 
   # Enable Eric
   use_eric_reverse_proxy    = true
   eric_port                 = "3001" # container port plus 1
-
-  # create a map of secret name => secret arn to pass into ecs service module
-  # using the trimprefix function to remove the prefixed path from the secret name
-  secrets_arn_map = {
-    for sec in data.aws_ssm_parameter.secret:
-      trimprefix(sec.name, "/${local.name_prefix}/") => sec.arn
-  }
 
   service_secrets_arn_map = {
     for sec in module.secrets.secrets:
